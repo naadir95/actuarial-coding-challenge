aa94fc483550716f49c4cbfebc7d4eef4475755d
Naadir Dawood <66675692+naadir-d@users.noreply.github.com>
2022-06-13 08:38:36 +0200

    Add teamMembers array to question file

--- src/question3.js
+++ src/question3.js
@@ -1,4 +1,59 @@
 // Question 3
+
+// Question 3.1
+const teamMembers = [
+  {
+    Name: "Burt",
+    Surname: "Verster",
+    idNumber: "9306185800085",
+    emailAddress: "burt@naked.insure"
+  },
+  {
+    Name: "Naadir",
+    Surname: "Dawood",
+    idNumber: "9509085800085",
+    emailAddress: "naadir@naked.insure"
+  },
+  {
+    Name: "Ricky",
+    Surname: "Smith",
+    idNumber: "9511035800082",
+    emailAddress: "ricky@naked.insure"
+  },
+  {
+    Name: "Chris",
+    Surname: "Cherry",
+    idNumber: "8905195800083",
+    emailAddress: "chris@naked.insure"
+  },
+  {
+    Name: "Philip",
+    Surname: "Rumpelt",
+    idNumber: "9506135800082",
+    emailAddress: "philip@naked.insure"
+  },
+  {
+    Name: "Erin",
+    Surname: "Rodrigues",
+    idNumber: "9602234800088",
+    emailAddress: "erin@naked.insure"
+  },
+  {
+    Name: "Ross",
+    Surname: "Ferguson",
+    idNumber: "8910175800087",
+    emailAddress: "ross@naked.insure"
+  },
+  {
+    Name: "Arne",
+    Surname: "Verster",
+    idNumber: "9710085800089",
+    emailAddress: "arne@naked.insure"
+  }
+];
+
+
+// Question 3.2
 var causeSelectionHistory = [
   {
     userEmail: "burt@naked.insure",
@@ -36,7 +91,9 @@ var causeSelectionHistory = [
     cause: "Green pop"
   }
 ];
+// Question 3.3
 
+// Question 3.4
 var claimsHistory = [
   {
     userEmail: "ricky@naked.insure",
